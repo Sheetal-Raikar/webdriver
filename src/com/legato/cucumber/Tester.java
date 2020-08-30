925c873ee49861180ada84007d1f029737ceeb13
Sheetal-Raikar <sheetalsraikar24@gmail.com>
2020-08-30 14:32:52 +0530

    sheetal committing one java file

---
+++ src/com/legato/cucumber/Tester.java
@@ -0,0 +1,15 @@
+package com.legato.cucumber;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("testing");
+		WebDriver driver = new ChromeDriver();
+
+	}
+
+}
